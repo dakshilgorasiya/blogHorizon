857795d6e64bfd0e5c0962047d0701c8202b15c0
dakshilgorasiya <dakshilgorasiya2005@gmail.com>
2025-02-23 15:54:53 +0530

    update header and footer components; replace favicon and add user menu functionality

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { Copyright } from "lucide-react";
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className="flex flex-col items-center justify-center p-3 bg-priary text-white text-sm">
+      <div className="flex items-center gap-2">
+        <Copyright size={20} />
+        <p>All rights reserved</p>
+      </div>
+      <p>Developed by Dakshil Gorasiya</p>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,68 +1,104 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import React, { useState } from "react";
+import { Search, CircleUser, LogOut } from "lucide-react";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 import { logout } from "../../features/auth/authReducers.js";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 
 function Header() {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
+  const user = useSelector((state) => state.auth.user);
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <>
-      <Link to="/">
-        <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700 mx-5">
-          HOME
-        </button>
-      </Link>
-      <Link to="/login">
-        <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700 mx-5">
-          Login
-        </button>
-      </Link>
-      <Link to="/register">
-        <button className="bg-blue-500 text-white px-4 py-2 mx-5 rounded hover:bg-blue-700">
-          Register
-        </button>
-      </Link>
-      <Link to="/forgot-password">
-        <button className="bg-blue-500 text-white px-4 py-2 mx-5 rounded hover:bg-blue-700">
-          Forgot Password
-        </button>
-      </Link>
-      <Link to="/reset-password/1245">
-        <button className="bg-blue-500 text-white px-4 py-2 mx-5 rounded hover:bg-blue-700">
-          Reset Password
-        </button>
-      </Link>
-      <Link to="/view-blog/67b22bcbe9b6867354a962ce">
-        <button className="bg-blue-500 text-white px-4 py-2 mx-5 rounded hover:bg-blue-700">
-          View Blog
-        </button>
-      </Link>
-      <button
-        className="bg-blue-500 text-white px-4 py-2 mx-5 rounded hover:bg-blue-700"
-        onClick={() => dispatch(logout())}
-      >
-        Logout
-      </button>
-      <Link to="/complete-profile">
-        <button className="bg-blue-500 text-white px-4 py-2 mx-5 rounded hover:bg-blue-700">
-          Complete Profile
-        </button>
-      </Link>
-      <Link to="/profile/67b1c637927d53d6d076742e">
-        <button className="bg-blue-500 text-white px-4 py-2 mx-5 rounded hover:bg-blue-700">
-          Profile
-        </button>
-      </Link>
-      <Link to="/create-blog">
-        <button className="bg-blue-500 text-white px-4 py-2 mx-5 rounded hover:bg-blue-700">
-          Create Blog
-        </button>
-      </Link>
-      <Link to="/edit-blog/67b22bcbe9b6867354a962ce">
-        <button className="bg-blue-500 text-white px-4 py-2 mx-5 rounded hover:bg-blue-700">
-          Edit Blog
-        </button>
-      </Link>
+      <div className="bg-priary flex justify-between items-center p-3">
+        <div className="p-3">
+          <Link to="/">
+            <img
+              src="https://res.cloudinary.com/bloghorizon/image/upload/v1740303181/wbiauijuf8e4gfz5m5xp.png"
+              alt="BlogHorizon"
+              className="h-5 md:h-10"
+            />
+          </Link>
+        </div>
+        <div className="bg-white flex items-center p-2 rounded-lg w-2/3">
+          <input
+            type="text"
+            placeholder="Search"
+            className="rounded-lg focus:outline-none w-full"
+          />
+          <button className="">
+            <Search size={20} />
+          </button>
+        </div>
+
+        <div className="p-3">
+          {!user ? (
+            <button
+              className="text-primary p-2 rounded-md"
+              onClick={() => navigate("/login")}
+            >
+              <CircleUser size={25} className="text-white" />
+            </button>
+          ) : (
+            <>
+              <button className="text-primary border-2 border-highlight rounded-full">
+                <img
+                  src={user.avatar}
+                  alt="avatar"
+                  onClick={handleClick}
+                  className="h-8 w-8 rounded-full"
+                />
+              </button>
+
+              <Menu
+                id="basic-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                  "aria-labelledby": "basic-button",
+                }}
+              >
+                <MenuItem onClick={handleClose}>
+                  <Link to={`/profile/${user._id}`}>
+                    <div className="flex items-center">
+                      <CircleUser size={20} className="mr-2" />
+                      Profile
+                    </div>
+                  </Link>
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                  <button
+                    onClick={() => {
+                      dispatch(logout());
+                      navigate("/");
+                    }}
+                  >
+                    <div className="flex items-center">
+                      <LogOut size={20} className="mr-2 text-red-500" />
+                      Logout
+                    </div>
+                  </button>
+                </MenuItem>
+              </Menu>
+            </>
+          )}
+        </div>
+      </div>
+
+      <div></div>
     </>
   );
 }
