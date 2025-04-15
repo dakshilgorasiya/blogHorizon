03d4020350608480ce9f2c6b074a903215ebe50f
Dakshil Gorasiya <151381252+dakshilgorasiya@users.noreply.github.com>
2025-04-15 18:14:13 +0530

    Update constants.js

--- backend/src/constants.js
+++ backend/src/constants.js
@@ -33,7 +33,6 @@ export const BLOG_CATEGORY = [
 
 export const COOKIE_CONFIG = {
   httpOnly: true,
-  sameSite: true,
   secure: process.env.NODE_ENV === "production",
   maxAge: 1000 * 60 * 60 * 24 * 15, // 15 days
 };
